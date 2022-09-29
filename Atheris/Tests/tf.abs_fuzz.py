f6014d61ae04fc815e0138bff81c83a8009e92a0
ShanHuang <sh69@illinois.edu>
2022-09-29 12:21:33 -0500

    add more type for tf.abs

--- Atheris/Tests/tf.abs_fuzz.py
+++ Atheris/Tests/tf.abs_fuzz.py
@@ -13,9 +13,17 @@ def TestOneInput(data):
     # tf.raw_ops.Abs takes tf.bfloat16, tf.float32, tf.float64, tf.int8, tf.int16,
     # tf.int32, tf.int64, tf.half but get_random_numeric_tensor only generates
     # tf.float16, tf.float32, tf.float64, tf.int32, tf.int64
-    input_tensor = fh.get_random_numeric_tensor()
-
-    _ = tf.abs(x=input_tensor)
+    try:
+        _DTYPES = [tf.bfloat16, tf.float32, tf.float64, tf.int32, tf.int64, tf.complex64, tf.complex128]
+        input_tensor = fh.get_random_numeric_tensor(dtype=fh.get_tf_dtype(allowed_set=_DTYPES))
+        intarg0 = fh.get_int()
+        floatarg0 = fh.get_float()
+        int_random = fh.get_int()
+        arg0_tensor = tf.identity(input_tensor)
+        arg0 = [intarg0,floatarg0,arg0_tensor]
+        _ = tf.abs(arg0[int_random%3])
+    except Exception as e:
+        pass
 
 
 def main():
